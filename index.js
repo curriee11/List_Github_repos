3b36f5db62e1c474cf28d5f39751fffc5390532e
Khushi Agarwal <khushiagarwal78910@gmail.com>
2023-01-19 10:42:19 +0530

    second commit'

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const form = document.getElementById('github-username-form');
 const loader = document.getElementById('loader');
 const repositoriesContainer = document.getElementById('repositories-container');
@@ -37,29 +36,57 @@ nextButton.addEventListener('click', () => {
 });
 
 const getRepositories = (username) => {
-  // API endpoint to fetch the repositories
-  const endpoint = `https://api.github.com/users/${username}/repos?page=${currentPage}&per_page=10`;
-  fetch(endpoint)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      totalPages = Math.ceil(data.length / 10);
-      renderRepositories(data);
-      loader.style.display = "none";
-      repositoriesContainer.style.display = "block";
-      handlePagination();
-    })
-    .catch((error) => {
-      console.error(error);
-      alert("Error occured while fetching the repositories")
-    });
-}
-
-const handlePagination = () => {
+    // API endpoint to fetch the repositories
+    const endpoint = `https://api.github.com/users/${username}/repos?page=${currentPage}&per_page=10`;
+    fetch(endpoint)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        totalPages = Math.ceil(data.length / 10);
+        renderRepositories(data);
+        loader.style.display = "none";
+        repositoriesContainer.style.display = "block";
+        handlePagination();
+      })
+      .catch((error) => {
+        console.error(error);
+        alert("Error occured while fetching the repositories. Please check the entered username and try again.");
+        loader.style.display = "none";
+      });
+  }
+  const renderRepositories = (data) => {
+    // Clear previous repositories
+    repositoriesContainer.innerHTML = "";
+    // Loop through the repositories and create elements to display them
+    for (let repo of data) {
+      const repoLink = document.createElement('a');
+      repoLink.href = repo.html_url;
+      repoLink.target = "_blank";
+      repoLink.textContent = repo.name;
+  
+      const repoName = document.createElement('h3');
+      repoName.appendChild(repoLink);
+  
+      const repoDescription = document.createElement('p');
+      repoDescription.textContent = repo.description;
+  
+      const repoLanguages = document.createElement('p');
+      repoLanguages.textContent = `Languages: ${repo.language}`;
+  
+      const repoItem = document.createElement('div');
+      repoItem.classList.add('repo-item');
+      repoItem.appendChild(repoName);
+      repoItem.appendChild(repoDescription);
+      repoItem.appendChild(repoLanguages);
+  
+      repositoriesContainer.appendChild(repoItem);
+    }
+  }
+  const handlePagination = () => {
     if (currentPage === 1) {
       prevButton.disabled = true;
       prevButton.style.display = "none";
@@ -76,4 +103,4 @@ const handlePagination = () => {
       nextButton.style.display = "inline-block";
     }
   }
-  
+      
