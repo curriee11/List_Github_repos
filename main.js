f76c9ba9fd09d60502f677764be25b5890ab3c37
Khushi Agarwal <khushiagarwal78910@gmail.com>
2023-02-11 12:05:44 +0530

    bio,locatio added

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const repoList = document.getElementById('repo-list');
 const pagination = document.getElementById('pagination');
 const loader = document.getElementById('loader');
 const userNameDisplay = document.getElementById('userNameDisplay');
-const image = document.getElementById('image');
+const bio = document.getElementById('bio');
 
 let currentPage = 0;
 let totalPages = 0;
@@ -55,8 +55,15 @@ const getRepositories = (username, page) => {
                 renderPagination(); 
                 // userNameDisplay.innerHTML = `<h2>${data.name}</h2>`;
                 // image.innerHTML=`<img src="${data.avatar_url}" style="display: block; margin: 0 auto; border-radius: 50%; width: 100px; height: 100px;">`;
-                userNameDisplay.innerHTML=` <img src="${data.avatar_url}" alt="Profile Picture">
-                <h2>${data.name}</h2>`;
+                userNameDisplay.innerHTML=` <div class="user-grid"><img src="${data.avatar_url}" alt="Profile Picture">
+                <div>
+                <h2>${data.name}</h2>
+                ${data.bio ? `<h5 class="bio"> ${data.bio}</h5>` : ''}
+                ${data.location ? `<div class="location"><i class="bi bi-geo-alt"></i><h5 class="location"> ${data.location}</h5></div>` : ''}
+                <h5 class="twitter">Twitter: <a href="https://twitter.com/${data.twitter_username}" target="_blank">${data.twitter_username}</a></h5>
+              </div></div>
+               `;
+            
                 // document.querySelector('#pagination button').classList.add('active');
 
                 console.log(data.public_repos);
